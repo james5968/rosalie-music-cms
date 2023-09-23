aab846a410aa4a39379c47dea961c1f8d1608d93
James Ashford <james.ashford@nandos.co.uk>
2023-09-23 15:52:34 +0100

    remove weird bits from admin

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,11 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link, graphql } from "gatsby";
+import { graphql } from "gatsby";
 import { getImage } from "gatsby-plugin-image";
 
 import Layout from "../components/Layout";
-import Features from "../components/Features";
-import BlogRoll from "../components/BlogRoll";
 import FullWidthImage from "../components/FullWidthImage";
 
 // eslint-disable-next-line
