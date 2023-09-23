912853b196173031a3025e3c32aca476a682bbc0
James Ashford <james.ashford@nandos.co.uk>
2023-09-23 16:28:35 +0100

    fix accessbility boo boos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,6 +20,7 @@ const Navbar = () => {
             className={`navbar-burger burger ${isActive && "is-active"}`}
             aria-expanded={isActive}
             onClick={() => setIsActive(!isActive)}
+            aria-label="Navigation drawer"
           >
             <span />
             <span />
--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -32,7 +32,7 @@ export const IndexPageTemplate = ({
                       <h1 className="title">{mainpitch.title}</h1>
                     </div>
                     <div className="tile">
-                      <h3 className="subtitle">{mainpitch.description}</h3>
+                      <h2 className="subtitle">{mainpitch.description}</h2>
                     </div>
                   </div>
                   <div className="columns">
